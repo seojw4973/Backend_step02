64726166060b8637c58846e1f22fa92c4849e6aa
seojw4973 <sgw0816@naver.com>
2024-04-12 15:13:17 +0900

    springex mybatis 실습

---
+++ springex/src/test/java/org/zerock/springex/sample/SampleTests.java
@@ -0,0 +1,47 @@
+package org.zerock.springex.sample;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+/* ServletContext : tomcat의 전역 영역, tomcat의 Container가 관리하는 영역
+*  ApplicationContext : Spring Container가 관리하는 영역, 이곳에 bean(관리되어지는 객체)를 저장한다.
+*  */
+@Log4j2
+@ExtendWith(SpringExtension.class)  //junit4 @RunWith
+
+// root-context.xml을 읽어들여서 ApplicationContext영역에 bean들을 생성한다.
+@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/root-context.xml")
+public class SampleTests {
+
+    // ApplicationContext에서 동일한 타입을 찾아서 자동으로 주입해라.
+    // 필드 주입 방식
+     @Autowired
+    private SampleService sampleService;
+
+     @Autowired
+     private DataSource dataSource;
+
+    @Test
+    public void textService1(){
+        log.info(sampleService);
+        Assertions.assertNotNull(sampleService);    //값의 유효여부 판단하는 메서드
+    }
+
+    @Test
+    public void textConnection() throws Exception{
+        Connection connection = dataSource.getConnection();
+        log.info(connection);
+        Assertions.assertNotNull(connection);
+
+        connection.close();
+    }
+
+}
